a3d1be09e7824f9406d80520540536ab2787210f
Roy Jou <36274083+TSPanda@users.noreply.github.com>
2018-05-18 17:33:33 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,22 +2,22 @@ const Discord = require('discord.js');
 const config = require('./config.json');
 
 const client = new Discord.Client();
-const prefix = config.prefixx;
+const prefix = config.prefix;
 
 client.on('ready', () => {
-  console.log('Loffed in as $client.user.tag}`);
-  client,user.setActivity(config.defaultActivity);
+  console.log(`Loffed in as $client.user.tag}`);
+  client.user.setActivity(config.defaultActivity);
 });
 
 client.on('message', msg => {
   if (message.author.bot) return;
-  if(message.content.indexOf(config.prefix) !==0) return;
+  if (message.content.indexOf(config.prefix) !==0) return;
   
-  const args = msg.content.dlice(config.prefix.length).trim().split(/ +/g);
+  const args = msg.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
   if (command === 'ping') {
-      msg.channelsend('Pong!');
+      msg.channel.send('Pong!');
   } else {
       return;
   }
