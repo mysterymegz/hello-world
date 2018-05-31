04c888ba2dcbc051e8f0fb237a4595ea19866576
mysterymegz <39407384+mysterymegz@users.noreply.github.com>
2018-05-31 04:03:21 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,7 +26,7 @@ client.on('message', msg => {
       .setTitle('Bot Commands ')
       .addField('Owner', 'mysterymegz')
       .addField('Prefix', '$')
-      .addField('Commands', 'slap, facedesk, ping, hug, eat, bed, pat, dance, highfive, hi, swat, hide, 8ball')
+      .addField('Commands', 'slap, facedesk, ping, hug, eat, bed, pat, poke, dance, highfive, hi, swat, hide, 8ball')
       .setColor(0x3958D0);
       msg.channel.sendEmbed(info);
   } 
