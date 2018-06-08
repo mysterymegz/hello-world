f225a01841e10d1e8cdae1846189b9653dc6e7a5
mysterymegz <39407384+mysterymegz@users.noreply.github.com>
2018-06-08 19:58:38 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,12 +39,12 @@ client.on('message', msg => {
                msg.channel.send("", {file: random.select('./textfile/cold')});
         }
 	
-	else if (command === 'love') {
-		msg.channel.send("", {file: random.select('./textfile/love')});
-	}
-	
 	else if (command === 'pun') {
 		msg.channel.send("", {file: random.select('./textfile/puns')});
+	}
+	
+	else if (command === 'love') {
+		msg.channel.send("", {file: random.select('./textfile/love')});
 	}
 	
 	else if (command === '8ball') {
